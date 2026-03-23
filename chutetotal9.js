4fb57c5ed3b4c0e9e05ccad8fddb86dbdd5b47c2
guilhermedesennfy-spec <guilherme.desen.nfy@gmail.com>
2026-03-23 17:11:00 -0300

    Update chutetotal9.js

--- chutetotal9.js
+++ chutetotal9.js
@@ -22,11 +22,11 @@ const scaleY = H / BASE_H;
 const scale = (Math.min(scaleX*1.5, scaleY*1.5));
 
 
-function degrau(x){
-    const e = Math.E ** (x * 0.2);
-    const k = -((((x * 0.2) - 10) / e) ** 2);
-    return (Math.E ** k) *1.0001; //9;//1.2;//1.6;//1.8801;
-}
+// function degrau(x){
+//     const e = Math.E ** (x * 0.2);
+//     const k = -((((x * 0.2) - 10) / e) ** 2);
+//     return (Math.E ** k) *1.0001; //9;//1.2;//1.6;//1.8801;
+// }
 
 const sprites = [];
 for (let i = 0; i < 13; i++) {
